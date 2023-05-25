50dc623ca539bc505d552d196c9fcb45dd4acd73
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-05-25 15:16:54 +0100

    commit

--- scripts/Black_jack.py
+++ scripts/Black_jack.py
@@ -1,3 +1,13 @@
 import random
 import time
 
+print('''
+Yb        dP 888888 88      dP""b8  dP"Yb  8b    d8 888888  
+ Yb  db  dP  88__   88     dP   `" dP   Yb 88b  d88 88__          
+  YbdPYbdP   88""   88  .o Yb      Yb   dP 88YbdP88 88""          
+   YP  YP    888888 88ood8  YboodP  YbodP  88 YY 88 888888 ''')
+print('''
+      888888  dP"Yb      88""Yb 88        db     dP""b8 88  dP  88888    db     dP""b8 88  dP 
+        88   dP   Yb     88__dP 88       dPYb   dP   `" 88odP      88   dPYb   dP   `" 88odP  
+        88   Yb   dP     88""Yb 88  .o  dP__Yb  Yb      88"Yb  o.  88  dP__Yb  Yb      88"Yb  
+        88    YbodP      88oodP 88ood8 dP""""Yb  YboodP 88  Yb "bodP' dP""""Yb  YboodP 88  Yb ''')
